6685df934f69485520252e8c70003e277cadcf91
RaniaRez <raniarezkellah2002@gmail.com>
2021-05-17 23:17:33 +0100

    Rania

--- lib/Bulles/BulleIcon.dart
+++ lib/Bulles/BulleIcon.dart
@@ -8,37 +8,8 @@ class BulleIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-        icon : Container(
-          child: Center(
-            child: Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                SizedBox(
-                    height: 400.0,
-                    width: 400.0,
-                    child: SvgPicture.asset(bulleWelcome)),
-                Positioned(
-                  top : 98.0,
-                  left: 50.5,
-                  right: 50.5,
-                  bottom: 10.0,
-                  child: Text(
-                    "BIENVENUE EN '     '",
-                    style: TextStyle(
-                      fontFamily: 'Skranji-Bold',
-                      fontSize: 30,
-                      color: Colors.brown,
-                      fontWeight: FontWeight.bold,
-
-                    ),
-                  ),
-                ),
-
-              ],
-            ),
-          ),),
-        iconSize: 200.0,
-
+        icon :
+        SvgPicture.asset(bulleWelcome),
         onPressed: onPressed
     );
   }
