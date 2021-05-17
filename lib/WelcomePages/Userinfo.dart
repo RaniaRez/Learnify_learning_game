0cc3d71550e11d44c58213e5f81876b0cb8879ec
Hanad-Nada <jn_hanad@esi.dz>
2021-05-18 00:28:27 +0100

    Nada

--- lib/WelcomePages/Userinfo.dart
+++ lib/WelcomePages/Userinfo.dart
@@ -30,182 +30,186 @@ class _UserinfoState extends State<Userinfo> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       body:
-      Stack(
-        children: <Widget>[
-          Container(
-            constraints: BoxConstraints.expand(),
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/forestbackground.jpg"),
-                    fit: BoxFit.cover)),
-          ),
-          Positioned(
-              top: size.height*0.05,
-              left:size.width*0.75,
-              child: SettingsButton(onPressed: (){
-                Navigator.push(
+      Container(
+        height: size.height,
+        width: size.width,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              constraints: BoxConstraints.expand(),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/forestbackground.jpg"),
+                      fit: BoxFit.cover)),
+            ),
+            Positioned(
+                top: size.height*0.05,
+                left:size.width*0.75,
+                child: SettingsButton(onPressed: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Settings()));
+                  print("HELL YEAH");
+                },)
+            ),
+
+            Positioned(
+                top: size.height*0.05,
+                right:size.width*0.75,
+                child: BacksButton(onPressed: (){
+                  print("HELL YEAH");
+                  Navigator.pop(
                     context,
-                    MaterialPageRoute(builder: (context) => Settings()));
-                print("HELL YEAH");
-              },)
-          ),
-
-          Positioned(
-              top: size.height*0.05,
-              right:size.width*0.75,
-              child: BacksButton(onPressed: (){
-                print("HELL YEAH");
-                Navigator.pop(
-                  context,
-                  );
-              },)
-          ),
-          Positioned(
-            top: size.height*0.047,
-            left: size.width*0.39,
-            child: HomeButton(
-              onPressed: (){
-                print("HELL YEAH");
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Home()),);
-
-              },
+                    );
+                },)
             ),
-          ),
-          Positioned(
-            top: size.height*0.3,
-            right:size.width*0.62,
-              child:BranchIconSimple(),
-            ),
-          if (New.avatar=="Pink")
-            Visibility(
+            Positioned(
+              top: size.height*0.047,
+              left: size.width*0.39,
+              child: HomeButton(
+                onPressed: (){
+                  print("HELL YEAH");
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),);
 
-              child: Positioned(
-                top: size.height*0.135,
-                right:size.width*0.65,
-                height: size.height*0.3,
-                width: size.width*0.3,
-                child:PinkAvatarIcon(onPressed: null,),
+                },
               ),
             ),
-          if (New.avatar=="Purple")
-            Visibility(
-
-              child: Positioned(
-                top: size.height*0.12,
-                right:size.width*0.65,
-                height: size.height*0.35,
-                width: size.width*0.35,
-                child:PurpleAvatarIcon(onPressed: null,),
+            Positioned(
+              top: size.height*0.3,
+              right:size.width*0.62,
+                child:BranchIconSimple(),
               ),
-            ),
-          if (New.avatar=="Orange")
-            Visibility(
-              child: Positioned(
-                top: size.height*0.14,
-                right:size.width*0.65,
-                height: size.height*0.3,
-                width: size.width*0.3,
-                child: OrangeAvatarIcon(onPressed: null,),
-              ),
-            ),
-          if (New.avatar=="Blue")
-            Visibility(
+            if (New.avatar=="Pink")
+              Visibility(
 
-              child: Positioned(
-                top: size.height*0.135,
-                right:size.width*0.65,
-                height: size.height*0.3,
-                width: size.width*0.3,
-                child:BlueAvatarIcon(onPressed: null,),
+                child: Positioned(
+                  top: size.height*0.135,
+                  right:size.width*0.65,
+                  height: size.height*0.3,
+                  width: size.width*0.3,
+                  child:PinkAvatarIcon(onPressed: null,),
+                ),
               ),
-            ),
+            if (New.avatar=="Purple")
+              Visibility(
 
-          Positioned(
-            top: size.height*0.65,
-            left:size.width*0.05,
-       child: Text(
+                child: Positioned(
+                  top: size.height*0.12,
+                  right:size.width*0.65,
+                  height: size.height*0.35,
+                  width: size.width*0.35,
+                  child:PurpleAvatarIcon(onPressed: null,),
+                ),
+              ),
+            if (New.avatar=="Orange")
+              Visibility(
+                child: Positioned(
+                  top: size.height*0.14,
+                  right:size.width*0.65,
+                  height: size.height*0.3,
+                  width: size.width*0.3,
+                  child: OrangeAvatarIcon(onPressed: null,),
+                ),
+              ),
+            if (New.avatar=="Blue")
+              Visibility(
+
+                child: Positioned(
+                  top: size.height*0.135,
+                  right:size.width*0.65,
+                  height: size.height*0.3,
+                  width: size.width*0.3,
+                  child:BlueAvatarIcon(onPressed: null,),
+                ),
+              ),
+
+            Positioned(
+              top: size.height*0.65,
+              left:size.width*0.05,
+         child: Text(
     'Supprimer le compte',
-           style: TextStyle(
-             fontFamily: 'Skranji-Bold',
-             fontSize: 24,
+             style: TextStyle(
+               fontFamily: 'Skranji-Bold',
+               fontSize: 24,
 
-             color:Colors.brown[700],
-           ),
-          ),
+               color:Colors.brown[700],
+             ),
+            ),
 
-          ),
+            ),
 Positioned(
   top:size.height*0.61,
   right:size.width*0.05,
-            child:ButtonSupp(
-              onPressed: (){
+              child:ButtonSupp(
+                onPressed: (){
 
-                print("SUPP");
-                },
+                  print("SUPP");
+                  },
+              ),
             ),
-          ),
-          Positioned(
-            top: size.height*0.45,
-            left:size.width*0.05,
-            child: Text(
-               'Changer avatar',
-                      style: TextStyle(
-                        fontFamily: 'Skranji-Bold',
-                        fontSize: 24,
+            Positioned(
+              top: size.height*0.45,
+              left:size.width*0.05,
+              child: Text(
+                 'Changer avatar',
+                        style: TextStyle(
+                          fontFamily: 'Skranji-Bold',
+                          fontSize: 24,
 
-                        color:Colors.brown[700],
-                      ),
-                     ),
-
-
-            ),
-          Positioned(
-            top:size.height*0.41,
-            right:size.width*0.05,
-            child: ButtonAvatar(
-              onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ChooseAvatar()),);
-                print("HELL YEAH");
-              },
-            ),
-          ),
-          Positioned(
-            top: size.height*0.55,
-            left:size.width*0.05,
-         child: Text(
-                      'Changer nom',
-                      style: TextStyle(
-                        fontFamily: 'Skranji-Bold',
-                        fontSize: 24,
-
-                        color:Colors.brown[700],
-                      ),
+                          color:Colors.brown[700],
                         ),
+                       ),
+
 
               ),
-
-          Positioned(
-            top:size.height*0.51,
-            right:size.width*0.05,
-            child: Container(
-
-              child: ButtonModifyNom(
+            Positioned(
+              top:size.height*0.41,
+              right:size.width*0.05,
+              child: ButtonAvatar(
                 onPressed: (){
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => Entrernom()),);
-
+                    MaterialPageRoute(builder: (context) => ChooseAvatar()),);
                   print("HELL YEAH");
                 },
               ),
             ),
-          ),
+            Positioned(
+              top: size.height*0.55,
+              left:size.width*0.05,
+           child: Text(
+                        'Changer nom',
+                        style: TextStyle(
+                          fontFamily: 'Skranji-Bold',
+                          fontSize: 24,
+
+                          color:Colors.brown[700],
+                        ),
+                          ),
+
+                ),
+
+            Positioned(
+              top:size.height*0.51,
+              right:size.width*0.05,
+              child: Container(
+
+                child: ButtonModifyNom(
+                  onPressed: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Entrernom()),);
+
+                    print("HELL YEAH");
+                  },
+                ),
+              ),
+            ),
 ],
-          ),
+            ),
+      ),
 
           );
 
