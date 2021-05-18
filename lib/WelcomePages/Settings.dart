0b802a269f483dbb49dfde72f07db71f3db2b673
Hanad-Nada <jn_hanad@esi.dz>
2021-05-18 14:38:33 +0100

    Nada

--- lib/WelcomePages/Settings.dart
+++ lib/WelcomePages/Settings.dart
@@ -5,6 +5,7 @@ import 'package:somthn/Buttons/buttonInfo.dart';
 import 'package:somthn/Buttons/buttonLightDark.dart';
 import 'package:somthn/Buttons/buttonMusic.dart';
 import 'package:somthn/Buttons/buttonSound.dart';
+import 'package:somthn/Buttons/buttonUserSettings.dart';
 import '../Buttons/BacksButton.dart';
 import 'Home.dart';
 
@@ -16,61 +17,66 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body:
-      Stack(
-          children: <Widget>[
-            Container(
-              constraints: BoxConstraints.expand(),
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("images/forestbackground.jpg"),
-                      fit: BoxFit.cover)),
+      Container(
+        height: size.height,
+        width: size.width,
+        child: Stack(
+            children: <Widget>[
+              Container(
+                constraints: BoxConstraints.expand(),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("images/forestbackground.jpg"),
+                        fit: BoxFit.cover)),
+              ),
+        Positioned(
+            top: size.height*0.5,
+            left:size.width*0.6,
+            child:MusicButton(onPressed:null,)
             ),
-Positioned(
-  top:380,
-  left: 250,
-  child:MusicButton(onPressed:null,)
-),
-            Positioned(
-                top:380,
-                left: 55,
-                child:SoundButton(onPressed:null,)
-            ),
-            Positioned(
-                bottom:80,
-                left:197,
-                child:LightDarkButton(onPressed:null,)
-            ),
-            Positioned(
-                bottom:80,
-                left:0,
-                child:HomeButton(onPressed:() {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Home()),);
-                })
-            ),
+              Positioned(
+                  top: size.height*0.5,
+                  right:size.width*0.6,
+                  child:SoundButton(onPressed:null,)
+              ),
+              Positioned(
+                  top: size.height*0.7,
+                  left:size.width*0.61,
+                  child:UserSettingsButton(onPressed:null,)
+              ),
+              Positioned(
+                  top: size.height*0.7,
+                  right:size.width*0.61,
+                  child:HomeButton(onPressed:() {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),);
+                  })
+              ),
 
-            Positioned(
-                top:140,
-                left:0,
-                child:BacksButton(onPressed:() {
+              Positioned(
+                  top: size.height*0.3,
+                  right:size.width*0.625,
+                  child:BacksButton(onPressed:() {
     print("HELL YEAH");
     Navigator.pop(context);
     },
-                )
+                  )
 
-            ),
-            Positioned(
-                top:180,
-                left:250,
-                child:InfoButton(onPressed:() {
-                  print("clicking");
-                })
-            ),
+              ),
+              Positioned(
+                  top: size.height*0.3,
+                  left:size.width*0.6,
+                  child:InfoButton(onPressed:() {
+                    print("clicking");
+                  })
+              ),
 
-          ]
+            ]
+        ),
       ),
     );
   }
