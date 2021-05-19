329fc0fba2e4717897f6f09cc616936eef509e75
RaniaRez <raniarezkellah2002@gmail.com>
2021-05-19 17:42:51 +0100

    Rania HADA WLID LKLB 8

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:somthn/Buttons/buttonInfo.dart';
 import 'package:somthn/Maths/BienvenueMath.dart';
 import 'package:somthn/Maths/I-M-1-1.dart';
+import 'package:somthn/Maths/I-M-1-2.dart';
 import 'package:somthn/TestDeNiveau/TestDeNiveau.dart';
 import 'package:somthn/WelcomePages/Home.dart';
 
 
 void main() {
   runApp(MaterialApp(
-    home:I_M_1_1_()
+    home:I_M_1_2_()
   ));
 }
