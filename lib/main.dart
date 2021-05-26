9f3f7598a237eb824c6bb2e18005b609f13097ad
Hanad-Nada <jn_hanad@esi.dz>
2021-05-26 21:13:12 +0100

    LAEZIZ FR

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:somthn/Francais/BienvenueFr.dart';
+import 'package:somthn/Francais/F-2-3.dart';
 import 'package:somthn/Francais/F-2.dart';
+import 'package:somthn/Francais/F-3.dart';
 import 'package:somthn/Maths/M-1.dart';
 
 
 void main() {
   runApp(MaterialApp(
-    home: Math1()
+    home: Fr3()
   ));
 }
