d4c08b5bad9866d9aad7b242887d24518d076b9f
RaniaRez <raniarezkellah2002@gmail.com>
2021-05-26 21:50:27 +0100

    RANIA HADA WLID LKLB 10

---
+++ lib/WelcomePages/Vite.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:somthn/Avatars/PurpleAvatarIcon.dart';
+import 'package:somthn/Branches/BranchIconSimple.dart';
+import 'package:somthn/Buttons/SinscrireButton.dart';
+import 'package:somthn/Buttons/seConnecterButton.dart';
+import 'package:somthn/WelcomePages/AllSet.dart';
+import 'package:somthn/WelcomePages/ChoixDomaines.dart';
+import 'package:somthn/Buttons/ClassementButton.dart';
+import 'package:somthn/WelcomePages/ChooseAvatar.dart';
+import 'package:somthn/classement/Classement.dart';
+import 'package:somthn/WelcomePages/Settings.dart';
+import 'package:somthn/WelcomePages/Userinfo.dart';
+import '../Buttons/settingsButton.dart';
+import '../Buttons/BacksButton.dart';
+import 'ClassUser.dart';
+import '../Buttons/buttonGoTo.dart';
+import '../Buttons/buttonStatistique.dart';
+import '../Buttons/buttonUserSettings.dart';
+//for backend
+//import '../Services/auth.dart';
+import '../Services/Login.dart';
+import '../Services/SignUp.dart';
+import '../Data/DataUser.dart';
+import '../classement/methodes.dart';
+import '../classement/Classement.dart';
+
+
+
+
+List<User> Utilisateurs = [];
+class Vite extends StatefulWidget {
+  @override
+  _ViteState createState() => _ViteState();
+}
+
+class _ViteState extends State<Vite> {
+  @override
+  bool _isVisible = false;
+
+  void show() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body:
+      Container(
+        height: size.height,
+        width: size.width,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              constraints: BoxConstraints.expand(),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/forestbackground.jpg"),
+                      fit: BoxFit.cover)),
+            ),
+            Positioned(
+                top: size.height*0.05,
+                left:size.width*0.75,
+                child: SettingsButton(onPressed: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Settings()));
+                  print("HELL YEAH");
+                },)
+            ),
+
+            Positioned(
+                top: size.height*0.05,
+                right:size.width*0.75,
+                child: BacksButton(onPressed: (){
+                  print("HELL YEAH");
+                  Navigator.pop(context);
+                },)
+            ),
+            Positioned(
+              top: size.height*0.05,
+              left: size.width*0.4,
+              child: ClassementButton(onPressed: ()  async {
+                List<DataUser> list = new List<DataUser>.empty(growable: true);
+                list = await classement();
+                list.sort((a, b) => b.score.compareTo(a.score));
+
+                print(list[0].score);
+                print(list[1].score);
+                print(list[2].score);
+                print("kemelna");
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Classement(value : list )));
+              }),
+            ),
+            Positioned(
+                top: size.height*0.25,
+                left: size.width*0.2,
+                height: size.height*0.35,
+                width: size.width*0.7,
+                child:SvgPicture.asset('assets/icons/bulleVite.svg'),
+
+            ),
+             Positioned(
+                height: size.height*0.2,
+                width: size.width*0.35,
+                top:size.height*0.5,
+                right:size.width*0.65,
+                child:BranchIconSimple(),
+              ),
+                Positioned(
+                  top: size.height*0.38,
+                  right: size.width*0.7 ,
+                  height: size.height*0.32,
+                  width: size.width*0.32,
+
+                  child:PurpleAvatarIcon(onPressed: null,),
+                ),
+
+
+            Positioned(
+              top: size.height*0.8,
+              right:size.width*0.75,
+                child: StatistiqueButton(onPressed: null,),
+
+              ),
+            Positioned(
+                top: size.height*0.8,
+                left:size.width*0.75,
+
+                child: GoToButton(onPressed: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ChooseAvatar()));
+
+                  print("HELL YEAH");
+                },
+                ),
+
+
+
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:somthn/Francais/BienvenueFr.dart';
 import 'package:somthn/Francais/F-1-1.dart';
 import 'package:somthn/Francais/F-1-3-2-OneC.dart';
 import 'package:somthn/Francais/F-1-3.dart';
+import 'package:somthn/WelcomePages/Vite.dart';
+import 'package:somthn/WelcomePages/Voila.dart';
 
 
 void main() {
   runApp(MaterialApp(
-    home:F_1_3()
+    home:Vite()
   ));
 }
