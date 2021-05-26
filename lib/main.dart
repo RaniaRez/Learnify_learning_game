14f21f7d178cf75b4cad098971845092cdfa64fe
RaniaRez <raniarezkellah2002@gmail.com>
2021-05-26 20:40:55 +0100

    RANIA HADA WLID LKLB 10

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:somthn/Francais/BienvenueFr.dart';
+import 'package:somthn/Francais/F-1-1.dart';
+import 'package:somthn/Francais/F-1-3-2-OneC.dart';
+import 'package:somthn/Francais/F-1-3.dart';
 
 
 void main() {
   runApp(MaterialApp(
-    home: BienvenueFr()
+    home:F_1_3()
   ));
 }
