a22276b8d02c8a6be44865dadf56c80efa9fc134
sarra_ben <js_bendaho@esi.dz>
2021-05-27 04:28:48 +0200

    delete added not working

---
+++ lib/Services/delete.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:somthn/WelcomePages/ClassUser.dart';
+import '../Data/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+/*final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+Future<void> delete() async  {
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount
+      .authentication;
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+      idToken: googleSignInAuthentication.idToken,
+      accessToken: googleSignInAuthentication.accessToken
+  );
+
+
+  final FirebaseUser user = authResult.user;
+  assert(!user.isAnonymous);
+  assert(await user.getIdToken() != null);
+  final AuthResult authResult = await user.reauthenticateWithCredential(credential);
+
+
+  await DatabaseService(uid: user.uid);  called from database class
+  await user.delete();
+
+  try {
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount
+        .authentication;
+    FirebaseUser user = await _auth.currentUser();
+    AuthCredential credentials =
+    GoogleAuthProvider.getCredential(      idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+    print(user);
+    AuthResult result = await user.reauthenticateWithCredential(credentials);
+    await DatabaseService(uid: result.user.uid).delete(); // called from database class
+    await result.user.delete();
+    return true;
+  } catch (e) {
+    print(e.toString());
+    return null;
+  }
+}*/
