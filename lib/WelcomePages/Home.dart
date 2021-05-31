8e79552800743ce58c08f5fe0dd4ee8afa230bfc
Hanad-Nada <jn_hanad@esi.dz>
2021-05-31 16:52:45 +0100

    FR DONE

--- lib/WelcomePages/Home.dart
+++ lib/WelcomePages/Home.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:somthn/WelcomePages/Vite.dart';
 import 'package:somthn/WelcomePages/Voila.dart';
+import 'package:somthn/WelcomePages/custom_dialog_box.dart';
 import '../Buttons/settingsButton.dart';
 import '../Buttons/ButtonAllons-y.dart';
 import '../Bulles/BulleIcon.dart';
@@ -90,7 +91,26 @@ class _HomeState extends State<Home> {
                 height: size.height*0.6,
                 width: size.width*0.7,
                 child:BulleIcon(onPressed: (){}),
+              ), Positioned(
+                  top: size.height*0.05,
+                  left:size.width*0.1,
+                  child:
+                  SettingsButton(onPressed: (){
+                    print("ff");
+                    showDialog(context: context,
+                        builder: (BuildContext context){
+                          return CustomDialogBox(
+                            title: "Custom Dialog Demo",
+                            descriptions: "Hii all this is a custom dialog in flutter and  you will be use in your flutter applications",
+                            text: "Yes",
+                          );
+                        }
+                    );
+                  },
+                  )
               ),
+
+
 
             ]
         ),
