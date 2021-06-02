9caa22acd13d8a84bf754fb2841e639b823a7d2c
RaniaRez <raniarezkellah2002@gmail.com>
2021-06-02 13:43:46 +0100

    RANIA HADA WLID LKLB 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,10 @@
 
 import 'package:flutter/material.dart';
-import 'package:somthn/Francais/F-1-4.dart';
-import 'package:somthn/Maths/M-1-2.dart';
-import 'package:somthn/Maths/M-2-1.dart';
-import 'package:somthn/Maths/M-2-4.dart';
-import 'package:somthn/Maths/M-2-5-2ndAttemptThreeclick.dart';
-import 'package:somthn/TestDeNiveau/TestNivFrQ3.dart';
-//import 'package:somthn/TestDeNiveau/TestNivMathQ1.dart';
-//import 'package:somthn/TestDeNiveau/TestNivMathQ2.dart';
-
-import 'package:somthn/TestDeNiveau/TestNivFrQ2.dart';
-import 'package:somthn/WelcomePages/Home.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:somthn/Francais/ScoreFr.dart';
+import 'package:somthn/Mutual/ScoreFr.dart';
+import 'package:somthn/Mutual/ScoreGeo.dart';
+import 'package:somthn/Mutual/ScoreMath.dart';
 import 'package:somthn/WelcomePages/Voila.dart';
-
-import 'WelcomePages/splash_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,5 +16,5 @@ void main() {
     }
   };
 
-  runApp(MaterialApp(home:Splash()));
+  runApp(MaterialApp(home: MathScore()));
 }
