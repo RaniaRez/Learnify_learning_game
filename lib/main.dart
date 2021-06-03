38ecce6dbb8fc14b469a3892d5e12096e39adc4a
mac <jh_achab@esi.dz>
2021-06-03 10:53:58 +0100

    za3maFiha

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:somthn/Mutual/ScoreFr.dart';
 import 'package:somthn/Mutual/ScoreGeo.dart';
 import 'package:somthn/Mutual/ScoreMath.dart';
+import 'package:somthn/Geographie/NiveauGeo.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+/*  WidgetsFlutterBinding.ensureInitialized();
   Map<String, dynamic> mots = {
     'manger': {
       'nombreDeLettre': 3,
       'lettres': ['m', 'a', 'n', 'g', 'e', 'r'],
       'lettresEntre': []
     }
-  };
-  runApp(MaterialApp(home: FrScore()));
+  };*/
+  runApp(MaterialApp(home: NiveauGeo()));
 }
