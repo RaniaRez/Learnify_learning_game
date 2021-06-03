82fb2d0d79533938dc32ac2926655ac2685acb71
Hanad-Nada <jn_hanad@esi.dz>
2021-06-03 16:07:14 +0100

    f this shit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
+
 import 'package:flutter/material.dart';
-import 'package:somthn/Mutual/ScoreFr.dart';
-import 'package:somthn/Mutual/ScoreGeo.dart';
-import 'package:somthn/Mutual/ScoreMath.dart';
-import 'package:somthn/Geographie/NiveauGeo.dart';
+import 'package:somthn/Mutual/Statistiques.dart';
+import 'package:somthn/WelcomePages/Home.dart';
+import 'Geographie/N3Q1.dart';
 
 void main() {
-/*  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   Map<String, dynamic> mots = {
     'manger': {
       'nombreDeLettre': 3,
       'lettres': ['m', 'a', 'n', 'g', 'e', 'r'],
       'lettresEntre': []
     }
-  };*/
-  runApp(MaterialApp(home: NiveauGeo()));
+  };
+
+  runApp(MaterialApp(home: N3Q1()));
 }
