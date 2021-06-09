e86cb91f3da50a0d24c13301efbe34f9d7d6dfe6
Hanad-Nada <jn_hanad@esi.dz>
2021-06-09 20:29:18 +0100

    f this shit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:somthn/Francais/F-1-1.dart';
+import 'package:somthn/WelcomePages/splash_screen.dart';
 void main() {
-  runApp(MaterialApp(home: F_1_1()));
+  runApp(MaterialApp(home: Splash()));
 }
